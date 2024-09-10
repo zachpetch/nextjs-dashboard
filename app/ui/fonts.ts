a4f79222e5f2ac2ccb7fd577d252b88847aa41b1
Zach Petch <zachpetch@gmail.com>
2024-09-09 21:49:42 -0700

    initial upload

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+import { Merriweather } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: "400" });
+export const merri = Merriweather({  subsets: ['latin'], weight: "400" });
