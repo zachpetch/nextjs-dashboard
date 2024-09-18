a98ce7ee2b3ba80bbf3fcdeac66fbaf3c60f0706
Zach Petch <zachpetch@gmail.com>
2024-09-18 16:53:45 -0700

    adding server-side form validation and accessibility improvements

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,16 +7,31 @@ import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
 
   /*
    * Note: if using a form with many fields, it may be more efficient to us the entries()
@@ -25,11 +40,22 @@ export async function createInvoice(formData: FormData) {
    * const rawFormData = Object.fromEntries(formData.entries())
    */
 
-  const { customerId, amount, status } = CreateInvoice.parse({
+  // Validate form fields with Zod
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to create invoice.',
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
@@ -40,7 +66,7 @@ export async function createInvoice(formData: FormData) {
     `;
   } catch (error) {
     return {
-      message: 'Database Error: Failed to create invoice.',
+      message: 'Database Error: Failed to create invoice.\n' + error,
     };
   }
 
@@ -48,13 +74,25 @@ export async function createInvoice(formData: FormData) {
   redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData
+) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
 
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to update invoice.',
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
 
   try {
@@ -65,7 +103,7 @@ export async function updateInvoice(id: string, formData: FormData) {
     `;
   } catch (error) {
     return {
-      message: 'Database Error: Failed to update invoice.',
+      message: 'Database Error: Failed to update invoice.\n' + error,
     };
   }
 
@@ -80,7 +118,7 @@ export async function deleteInvoice(id: string) {
     return { message: 'Invoice delete.' };
   } catch (error) {
     return {
-      message: 'Database Error: Failed to delete invoice.',
+      message: 'Database Error: Failed to delete invoice.\n' + error,
     };
   }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
-import { lusitana, merri } from '@/app/ui/fonts';
+import { merri } from '@/app/ui/fonts';
 import Image from 'next/image';
 
 export default function Page() {
