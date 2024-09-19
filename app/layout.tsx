5c2a87958044093cac9f3cf9b6b2424cdff593a8
Zach Petch <zachpetch@gmail.com>
2024-09-19 11:50:20 -0700

    Adding metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'A simple example of a Next.js web app',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
@@ -8,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head><title>NextJS Dashboard</title></head>
+      {/* <head><title>NextJS Dashboard</title></head> */}
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
